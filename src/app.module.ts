c72447e817b6b0acc4c7406b5d1d3c568ba21800
Efisio Pitzalis Morillas <git.epitzalis@gmx.com>
2020-05-04 19:56:14 +0200

    install Type Orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookController } from './book/book.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'http://192.168.64.2',
+      port: 3306,
+      username: 'nest',
+      password: '1234',
+      database: 'bookapp_db',
+      entities: [ __dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, BookController],
   providers: [AppService],
 })
