6035d9a66ae2f9686c219c81fcc8aa7170a0db14
Efisio Pitzalis Morillas <git.epitzalis@gmx.com>
2020-05-16 23:50:40 +0200

    update entity book

--- src/book/book.entity.ts
+++ src/book/book.entity.ts
@@ -3,16 +3,19 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 @Entity()
 export class Book {
 
+  // @PrimaryGeneratedColumn('uuid')
+  // readonly id: string;
+
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 150 })
+  @Column({ length: 150, nullable: true })
   name: string;
 
   @Column({ length: 150 })
   author: string;
 
-  @Column({ length: 20 })
+  @Column({ length: 20, unique: true })
   isbn: string;
 
   @Column({ nullable: true })
